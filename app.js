b23dcdbf612db0b9e0f5c0f68e53b6023e8dd129
kiranrokkam09 <kiran.rokkam456@gmail.com>
2023-11-08 23:55:54 +0530

    Laying the foundation

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-const heading = React.createElement('h1', { id: "heading" }, "Hello World from React!");
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+const Head=()=>(<h1 className='heading'>Namaste React using JSX!</h1>)
+
+const FunComponent=()=>(<div id='container'>
+    <Head/>
+    <h1>
+        Namaste React Inside Functional Component!
+    </h1>
+</div>)
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<FunComponent/>)
